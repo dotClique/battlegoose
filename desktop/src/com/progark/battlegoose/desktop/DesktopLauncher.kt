5503dd768fa60003bf482ebd8ff3d85645e76655
Ulrik Røsby <ulrik@rosby.no>
2022-03-16 08:53:09 +0100

    Initial commit

---
+++ desktop/src/com/progark/battlegoose/desktop/DesktopLauncher.kt
@@ -0,0 +1,14 @@
+package com.progark.battlegoose.desktop
+
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.progark.battlegoose.Game
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
