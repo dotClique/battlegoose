c9b7d077a1526e00d2f86609cec70bfce91a3949
Ulrik Røsby <ulrik@rosby.no>
2022-03-17 23:01:49 +0100

    feat: add basic MainMenuState

---
+++ core/src/gamestates/MainMenuState.kt
@@ -0,0 +1,37 @@
+package gamestates
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class MainMenuState
+    : GameState() {
+
+    private val background = Texture("heli1.png")
+    private val playBtn = Texture("play.png")
+
+    private fun handleInput() {
+        if (Gdx.input.justTouched())
+            GameStateManager.push(GameStates.BATTLE_STATE)
+    }
+
+    override fun update(dt: Float) {
+        handleInput()
+    }
+
+    override fun render(sb: SpriteBatch) {
+        val middleX =  Gdx.graphics.width/2f  - playBtn.width / 2f
+        val middleY = Gdx.graphics.height/2f  - playBtn.height/2
+        sb.begin()
+        sb.draw(background, 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        sb.draw(playBtn, middleX, middleY)
+        sb.end()
+    }
+
+    override fun dispose() {
+        background.dispose()
+        playBtn.dispose()
+    }
+
+
+}
