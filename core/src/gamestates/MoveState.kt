b3bc403182c690ffda2f65e34305b8121a692d43
Ulrik Røsby <ulrik@rosby.no>
2022-03-17 23:09:08 +0100

    feat: add todo class definitions of the rest of the planned gamestates

---
+++ core/src/gamestates/MoveState.kt
@@ -0,0 +1,17 @@
+package gamestates;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+public class MoveState: GameState(){
+    override fun update(dt: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun render(sb: SpriteBatch) {
+        TODO("Not yet implemented")
+    }
+
+    override fun dispose() {
+        TODO("Not yet implemented")
+    }
+}
