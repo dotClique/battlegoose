5ea7eb9436909c6bad57df37da32d7fb0b2d8d79
Ulrik Røsby <ulrik@rosby.no>
2022-03-17 23:09:41 +0100

    feat: update Game class to run using the GameStateManager

--- core/src/com/progark/battlegoose/Game.kt
+++ core/src/com/progark/battlegoose/Game.kt
@@ -1,27 +1,31 @@
 package com.progark.battlegoose
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
+import gamestates.GameStateManager
+import gamestates.GameStates
 
 class Game : ApplicationAdapter() {
     private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        GameStateManager.push(GameStates.MAIN_MENU)
     }
 
     override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        ScreenUtils.clear(1f, 1f, 1f, 1f)
+        GameStateManager.update(Gdx.graphics.deltaTime)
+        GameStateManager.render(batch)
+
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
     }
+
+
 }
