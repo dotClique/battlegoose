dd33bc6bc5908c612dc4d55015cfd54f4fa875e9
Ulrik Røsby <ulrik@rosby.no>
2022-03-18 12:46:35 +0100

    feat: add ControllerBase abstract methods and attributes

--- core/src/controllers/ControllerBase.kt
+++ core/src/controllers/ControllerBase.kt
@@ -1,4 +1,10 @@
 package controllers
 
-class ControllerBase {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import views.ViewBase
+
+abstract class ControllerBase(private val model: Any, private val view: ViewBase) {
+    abstract fun update(dt: Double)
+    abstract fun render(sb: SpriteBatch)
+    abstract fun dispose()
 }
