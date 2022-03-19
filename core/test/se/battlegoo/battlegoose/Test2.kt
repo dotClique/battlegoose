145b1496e087227989a31de76d1475fa0fda1897
Lars Mitsem Selbekk <lars@selbekk.com>
2022-03-19 03:12:02 +0100

    Add unit test infrastructure

---
+++ core/test/se/battlegoo/battlegoose/Test2.kt
@@ -0,0 +1,16 @@
+package se.battlegoo.battlegoose
+
+import org.junit.Test
+
+class Test2 {
+    @Test
+    fun test() {
+        assert(true)
+    }
+
+    @Test
+    fun banana() {
+        Game()
+        assert(true)
+    }
+}
