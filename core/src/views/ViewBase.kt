0cb4b42532d0f9ee5cafb33bdd50346def788c80
Ulrik Røsby <ulrik@rosby.no>
2022-03-22 12:37:26 +0100

    fix: separate UnitViewBase from ViewBase

--- core/src/views/ViewBase.kt
+++ core/src/views/ViewBase.kt
@@ -6,41 +6,8 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 
-abstract class ViewBase(
-    texturePath: String,
-    protected val textureStartFacingDirection: FacingDirection = FacingDirection.RIGHT
-) {
-
-    protected val texture = Texture(texturePath)
-    protected var sprite = Sprite(texture)
-    protected var facingDirection = textureStartFacingDirection
-
-    fun clicked(): Boolean {
-        if (!Gdx.input.justTouched()) return false
-        val touchPoint = Vector2(Gdx.input.x.toFloat(), Gdx.input.y.toFloat())
-        val thisRect = sprite.boundingRectangle
-        if (thisRect.contains(touchPoint)) return true
-        return false
-    }
-
-    fun overlaps(other: ViewBase): Boolean {
-        val thisRect = this.sprite.boundingRectangle
-        val otherRect = other.sprite.boundingRectangle
-        return thisRect.overlaps(otherRect)
-    }
-
-    fun setDirection(newDirection: FacingDirection) {
-        if (newDirection == this.facingDirection) return;
-        facingDirection = newDirection
-        sprite.flip(true, false)
-    }
-
-    open fun render(sb: SpriteBatch) {
-        sprite.draw(sb)
-    }
-
-    open fun dispose() {
-        texture.dispose()
-    }
+abstract class ViewBase() {
+    abstract fun render(sb: SpriteBatch)
+    abstract fun dispose()
 
 }
