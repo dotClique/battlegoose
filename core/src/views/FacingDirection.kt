9a76b375fd6bd4e749c4167328329d6b0bb5bcc8
Ulrik Røsby <ulrik@rosby.no>
2022-03-22 10:07:34 +0100

    feat: add basic ViewBase

---
+++ core/src/views/FacingDirection.kt
@@ -0,0 +1,6 @@
+package views
+
+enum class FacingDirection {
+    RIGHT,
+    LEFT
+}
--- core/src/views/ViewBase.kt
+++ core/src/views/ViewBase.kt
@@ -1,4 +1,46 @@
 package views
 
-class ViewBase {
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+
+abstract class ViewBase(
+    texturePath: String,
+    protected val textureStartFacingDirection: FacingDirection = FacingDirection.RIGHT
+) {
+
+    protected val texture = Texture(texturePath)
+    protected var sprite = Sprite(texture)
+    protected var facingDirection = textureStartFacingDirection
+
+    fun clicked(): Boolean {
+        if (!Gdx.input.justTouched()) return false
+        val touchPoint = Vector2(Gdx.input.x.toFloat(), Gdx.input.y.toFloat())
+        val thisRect = sprite.boundingRectangle
+        if (thisRect.contains(touchPoint)) return true
+        return false
+    }
+
+    fun overlaps(other: ViewBase): Boolean {
+        val thisRect = this.sprite.boundingRectangle
+        val otherRect = other.sprite.boundingRectangle
+        return thisRect.overlaps(otherRect)
+    }
+
+    fun setDirection(newDirection: FacingDirection) {
+        if (newDirection == this.facingDirection) return;
+        facingDirection = newDirection
+        sprite.flip(true, false)
+    }
+
+    fun render(sb: SpriteBatch) {
+        sprite.draw(sb)
+    }
+
+    fun dispose() {
+        texture.dispose()
+    }
+
 }
