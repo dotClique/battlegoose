bda509854ec6b86bc7aa502aadae6081ac77f9ee
Ulrik Røsby <ulrik@rosby.no>
2022-03-22 10:11:43 +0100

    feat: implement some standard methods of ControllerBase

--- core/src/controllers/ControllerBase.kt
+++ core/src/controllers/ControllerBase.kt
@@ -5,6 +5,10 @@ import views.ViewBase
 
 abstract class ControllerBase(private val model: Any, private val view: ViewBase) {
     abstract fun update(dt: Double)
-    abstract fun render(sb: SpriteBatch)
-    abstract fun dispose()
+    open fun render(sb: SpriteBatch) {
+        view.render(sb)
+    }
+    open fun dispose() {
+        view.dispose()
+    }
 }
