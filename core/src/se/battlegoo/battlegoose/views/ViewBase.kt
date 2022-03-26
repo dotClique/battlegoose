a7f080afac618bde51002ed5785e9d75ddea2603
Ulrik Røsby <ulrik@rosby.no>
2022-03-26 11:29:26 +0100

    fix: random formatting

--- core/src/se/battlegoo/battlegoose/views/ViewBase.kt
+++ core/src/se/battlegoo/battlegoose/views/ViewBase.kt
@@ -2,7 +2,7 @@ package se.battlegoo.battlegoose.views
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-abstract class ViewBase() {
+abstract class ViewBase {
     abstract fun render(sb: SpriteBatch)
     abstract fun dispose()
 }
