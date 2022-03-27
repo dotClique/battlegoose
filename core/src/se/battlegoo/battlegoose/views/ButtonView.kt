85f29f914804befb576466579f9edd04db59887e
Anonymous <anonymous@stud.ntnu.no>
2022-03-27 19:02:00 +0200

    Support scaling, overlap detection and positioning in ButtonView.kt

--- core/src/se/battlegoo/battlegoose/views/ButtonView.kt
+++ core/src/se/battlegoo/battlegoose/views/ButtonView.kt
@@ -6,25 +6,48 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
+import kotlin.properties.Delegates
 
-class ButtonView(texturePath: String): ViewBase() {
+
+class ButtonView(val cam: OrthographicCamera, texturePath: String,
+                 private val xPos: Float,
+                 private val yPos: Float,
+                 private val scale: Float = 1f
+): ViewBase() {
 
     private val buttonTexture = Texture(texturePath)
     private val buttonTextureRegion = TextureRegion(buttonTexture)
-    private val button = Sprite(buttonTextureRegion)
+    private val button = Rectangle(
+        buttonTextureRegion.regionX.toFloat(),
+        buttonTextureRegion.regionY.toFloat(),
+        buttonTextureRegion.regionWidth.toFloat(),
+        buttonTextureRegion.regionHeight.toFloat()
+    )
 
+    val btnWidth: Float = buttonTextureRegion.regionWidth*scale
+    val btnHeight: Float = buttonTextureRegion.regionHeight*scale
 
+    /**
+     * Handle whether a button is pressed or not
+     *
+     * @return boolean
+     */
     fun isPressed(): Boolean {
         return if (Gdx.input.justTouched()) {
-            val position = Vector2(Gdx.input.x.toFloat(), Gdx.input.y.toFloat())
-            val buttonBounds = button.boundingRectangle
-            buttonBounds.contains(position)
+            val yInverted = Gdx.graphics.height - Gdx.input.y.toFloat() // Because of different axis directions
+
+            val tmp = Vector2(Gdx.input.x.toFloat(), yInverted)
+            val position = gdxToCam(tmp.x, tmp.y)
+
+            button.contains(position)
         } else false
     }
 
     override fun render(sb: SpriteBatch) {
-        sb.draw(buttonTextureRegion, 0f, 0f)
+        sb.draw(buttonTextureRegion, xPos, yPos, this.btnWidth, this.btnHeight)
     }
 
     override fun dispose() {
@@ -33,12 +56,11 @@ class ButtonView(texturePath: String): ViewBase() {
 
     /**
      * Public function that scales Gdx values to Viewport values
-     *
+     */
     fun gdxToCam(x: Float, y: Float): Vector2 {
         return Vector2(
             x / Gdx.graphics.width.toFloat() * cam.viewportWidth,
             y / Gdx.graphics.height.toFloat() * cam.viewportHeight
         )
     }
-    **/
 }
