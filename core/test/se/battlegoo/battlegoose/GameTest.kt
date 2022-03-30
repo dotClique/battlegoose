44d520eb1d098bac6b6d0a7fdd650e0f7e206b0d
Anonymous <anonymous@stud.ntnu.no>
2022-03-30 16:39:39 +0200

    Remove unused imports from faulty test

--- core/test/se/battlegoo/battlegoose/GameTest.kt
+++ core/test/se/battlegoo/battlegoose/GameTest.kt
@@ -1,8 +1,6 @@
 package se.battlegoo.battlegoose
 
 import org.junit.Test
-import se.battlegoo.battlegoose.gamestates.GameStateManager
-import se.battlegoo.battlegoose.gamestates.LeaderboardState
 
 class GameTest {
     @Test
