6f63d5e20756d28b643e0e6e030a36db03af5b0c
Anonymous <anonymous@stud.ntnu.no>
2022-04-11 16:39:30 +0200

    Implement an example of create lobby with id

--- core/src/se/battlegoo/battlegoose/gamestates/CreateLobbyState.kt
+++ core/src/se/battlegoo/battlegoose/gamestates/CreateLobbyState.kt
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
 import se.battlegoo.battlegoose.Game
 import se.battlegoo.battlegoose.views.CreateLobbyView
 
@@ -19,6 +20,10 @@ class CreateLobbyState : GameState() {
 
     private val createLobbyView: CreateLobbyView = CreateLobbyView()
 
+    private val title: BitmapFont = BitmapFont()
+    private val titleText = "CREATE LOBBY"
+    private val layoutTitle = GlyphLayout(title, titleText)
+
     private val goBack: BitmapFont = BitmapFont()
     private val goBackText = "Press anywhere to return to main menu..."
     private val layoutGoBack = GlyphLayout(goBack, goBackText)
@@ -29,21 +34,30 @@ class CreateLobbyState : GameState() {
     }
 
     override fun update(dt: Float) {
-        handleInput()
+        //handleInput()
     }
 
     override fun render(sb: SpriteBatch) {
         createLobbyView.render(sb)
 
+        title.data.setScale(5f)
+        title.draw(
+            sb, titleText, (Game.WIDTH / 2f) - (layoutTitle.width * 5f / 2f),
+            (Game.HEIGHT * 0.9f) + layoutTitle.height * 3f
+        )
+
         goBack.data.setScale(3f)
+        /*
         goBack.draw(
             sb, goBackText, cam.viewportWidth / 20f - (layoutGoBack.width / 3f),
             cam.viewportHeight / 20f + layoutGoBack.height * 3f
         )
+         */
     }
 
     override fun dispose() {
         createLobbyView.dispose()
+        title.dispose()
         goBack.dispose()
     }
 }
--- core/src/se/battlegoo/battlegoose/views/CreateLobbyView.kt
+++ core/src/se/battlegoo/battlegoose/views/CreateLobbyView.kt
@@ -3,28 +3,52 @@ package se.battlegoo.battlegoose.views
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle
+import com.badlogic.gdx.utils.Align
+import java.util.*
 
 class CreateLobbyView : ViewBase() {
 
-    private val background = Texture("createLobbyBackground.png")
+    private val background = Texture("menuBackground.jpg")
 
-    private var skin: Skin = Skin(Gdx.files.internal("uiskin.json"))
+    private var skin: Skin = Skin(Gdx.files.internal("star-soldier-ui.json"))
     private var textField: TextField = TextField("", skin)
+    private var textFieldStyle = skin.get(TextFieldStyle::class.java)
+
+    private val stage = Stage()
+
+    private var uuid = UUID.randomUUID()
 
     init {
         textField.messageText = "Enter an ID for your lobby"
-        textField.setPosition(0f, 0f)
+        textField.alignment = Align.center
+        textFieldStyle.font.data.setScale(2.4f)
+        textField.height = Gdx.graphics.height.toFloat() / 10
+        textField.width = Gdx.graphics.width.toFloat() / 2
+        textField.setPosition(Gdx.graphics.width.toFloat() / 2 - textField.width / 2, Gdx.graphics.height.toFloat() / 1.5f)
+        stage.addActor(textField)
+        Gdx.input.inputProcessor = stage
+        generateRandomLobbyId(textField)
     }
 
     //Gdx.input.setOnscreenKeyboardVisible(true);
 
     override fun render(sb: SpriteBatch) {
         sb.draw(background, 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        textField.draw(sb, 1f)
+        stage.draw()
+        stage.act()
     }
 
     override fun dispose() {
         background.dispose()
+    }
+
+    private fun generateRandomLobbyId(textField: TextField) {
+        textField.text = uuid.toString().slice(0..15)
     }
 }
