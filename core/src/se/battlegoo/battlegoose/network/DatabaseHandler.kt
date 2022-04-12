6d59302409541822c76aa1f30fcba68389ca92bd
Ulrik Røsby <ulrik@rosby.no>
2022-04-12 15:02:00 +0200

    feat: add when-path to read BattleData from readReferenceValue

--- core/src/se/battlegoo/battlegoose/network/DatabaseHandler.kt
+++ core/src/se/battlegoo/battlegoose/network/DatabaseHandler.kt
@@ -1,11 +1,18 @@
 package se.battlegoo.battlegoose.network
 
-import java.util.function.Consumer
+import com.badlogic.gdx.utils.Logger
 import pl.mk5.gdx.fireapp.GdxFIRAuth
 import pl.mk5.gdx.fireapp.GdxFIRDatabase
 import pl.mk5.gdx.fireapp.auth.GdxFirebaseUser
+import pl.mk5.gdx.fireapp.database.FilterType
+import pl.mk5.gdx.fireapp.database.OrderByMode
 import pl.mk5.gdx.fireapp.promises.Promise
+import se.battlegoo.battlegoose.datamodels.ActionData
+import se.battlegoo.battlegoose.datamodels.BattleData
 import se.battlegoo.battlegoose.datamodels.LobbyData
+import java.util.function.Consumer
+
+typealias ConversionFunc<T> = (HashMap<String, Any>) -> T
 
 class DatabaseHandler {
     fun signInAnonymously(): Promise<GdxFirebaseUser> {
@@ -18,6 +25,21 @@ class DatabaseHandler {
         }
     }
 
+    inline fun <reified T : Any> readFilteredValue(
+        databasePath: String,
+        order: OrderByMode,
+        orderByArg: String,
+        filter: FilterType,
+        filterArg: String,
+        consumer: Consumer<T?>,
+    ) {
+        GdxFIRDatabase.inst().inReference(databasePath)
+            .filter(filter, filterArg).orderBy(order, orderByArg)
+            .readValue(T::class.java).then<T> {
+                consumer.accept(it)
+            }
+    }
+
     inline fun <reified T : Any> readPrimitiveValue(databasePath: String, consumer: Consumer<T?>) {
         GdxFIRDatabase.inst().inReference(databasePath).readValue(T::class.java).then<T> {
             consumer.accept(it)
@@ -26,19 +48,35 @@ class DatabaseHandler {
 
     inline fun <reified T : Any> readReferenceValue(databasePath: String, consumer: Consumer<T?>) {
         when (T::class) {
-            LobbyData::class ->
-                    readPrimitiveValue<HashMap<String, Any>>(
-                            databasePath,
-                            Consumer { data ->
-                                if (data == null) {
-                                    consumer.accept(null)
-                                    return@Consumer
-                                }
-                                consumer.accept(convertToLobby(data) as T)
-                            }
-                    )
-        // TODO: BattleData class
+            LobbyData::class -> readDataClass(
+                databasePath,
+                consumer,
+                this::convertToLobby as ConversionFunc<T>
+            )
+            BattleData::class -> readDataClass(
+                databasePath,
+                consumer,
+                this::convertToBattle as ConversionFunc<T>
+            )
         }
+    }
+
+
+    fun <T> readDataClass(
+        databasePath: String,
+        consumer: Consumer<T?>,
+        conversionFunc: ConversionFunc<T>
+    ) {
+        return readPrimitiveValue<HashMap<String, Any>>(
+            databasePath,
+            Consumer { consumerData ->
+                if (consumerData == null) {
+                    consumer.accept(null);
+                    return@Consumer
+                }
+                consumer.accept(conversionFunc(consumerData))
+            })
+
     }
 
     fun setValue(databasePath: String, value: Any): Promise<Void> {
@@ -49,10 +87,23 @@ class DatabaseHandler {
         return GdxFIRDatabase.inst().inReference(databasePath).push().setValue(value)
     }
 
+
     fun convertToLobby(data: HashMap<String, Any>): LobbyData {
         val hostID = data["hostID"].toString()
         val otherPlayerID = data["otherPlayerID"].toString()
         val shouldStart = data["shouldStart"].toString().toBoolean()
         return LobbyData(hostID, otherPlayerID, shouldStart)
+    }
+
+    fun convertToBattle(battleData: HashMap<String, Any>): BattleData {
+        val hostID = battleData["hostID"].toString()
+        val otherPlayerID = battleData["otherPlayerID"].toString()
+        val actionsSome = battleData["actions"]
+
+        val logger = Logger("ulrik")
+        logger.error("ActionsSome: $actionsSome, type: ${actionsSome!!::class}")
+
+        val actions = listOf<ActionData>()
+        return BattleData(hostID, otherPlayerID, actions)
     }
 }
