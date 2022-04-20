68fed93a25e2e534b010861f5cb1eab23da20c66
Ulrik Røsby <ulrik@rosby.no>
2022-04-20 16:29:23 +0200

    feat: make Stage a global variable that is replaced with each new GameState

--- core/src/se/battlegoo/battlegoose/gamestates/GameState.kt
+++ core/src/se/battlegoo/battlegoose/gamestates/GameState.kt
@@ -1,8 +1,20 @@
 package se.battlegoo.battlegoose.gamestates
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import se.battlegoo.battlegoose.Game
 
 abstract class GameState {
+    init {
+        initialize()
+    }
+
+    fun initialize() {
+        Game.stage = Stage(Game.viewPort, Game.batch)
+        Gdx.input.inputProcessor = Game.stage
+    }
+
     abstract fun update(dt: Float)
     abstract fun render(sb: SpriteBatch)
     abstract fun dispose()
